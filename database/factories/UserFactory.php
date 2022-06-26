0cc43d9700335ef0467b0a5d56ea323eed997532
Matheus Oliveira <matheus.oliveira110101@gmail.com>
2022-06-26 18:24:48 -0300

    new docker-compose config

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,6 +17,8 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+
+        
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
